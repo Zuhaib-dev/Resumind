e6ab5afdfacdc313a08aa499409f23a2a389aa1f
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-09-22 12:27:54 +0530

    Added google-search-console meata tag link in root.tsx for verficaition

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* ✅ Google Search Console verification */}
+        <meta
+          name="google-site-verification"
+          content="terZRkP5xAisMxUTVWZ_rW6MXSuOeuAFITryD0CBDxA"
+        />
+
         <Meta />
         <Links />
       </head>
